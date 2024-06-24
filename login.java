4f2fa5befd2eda0a6dfa86ec8342c079326c2d27
Maithili <deshmukh.maithili23@gmail.com>
2024-06-24 11:35:26 +0530

    Update the file

--- login.java
+++ login.java
@@ -1,10 +1,3 @@
-Hello Java World!!
-My Name is Maithili Deshmukh :)
+Hiii Piyush Its Maithili
 
-My Goal is to get a job in IT with a good package..
-  I also want my Man Mr. Piyush Buradkar to get whatever he wishes to have..And Bless his parents Tyanna sukh aani aarogya de deva
-  Please Deva tyachi pratyek wish purn kara..
-
-  Majhya Mummy Pappa bhau la sukhi thewa...Ayush la tyacha ayushyat khup motha kara..
-  tumhi mala je pn kahi dilay tya sathi mi khup grateful aahe..
-  Thankyou deva!!!!
+I Miss You>>
