6e8876f9357cebe9615ab9120ae670466263388a
Git-Maithili <165825588+Git-Maithili@users.noreply.github.com>
2024-06-24 11:28:04 +0530

    New Text added

--- login.java
+++ login.java
@@ -1,2 +1,10 @@
 Hello Java World!!
 My Name is Maithili Deshmukh :)
+
+My Goal is to get a job in IT with a good package..
+  I also want my Man Mr. Piyush Buradkar to get whatever he wishes to have..And Bless his parents Tyanna sukh aani aarogya de deva
+  Please Deva tyachi pratyek wish purn kara..
+
+  Majhya Mummy Pappa bhau la sukhi thewa...Ayush la tyacha ayushyat khup motha kara..
+  tumhi mala je pn kahi dilay tya sathi mi khup grateful aahe..
+  Thankyou deva!!!!
